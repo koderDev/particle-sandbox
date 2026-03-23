4d0e5a445bd3635f92df90013421847b1bc8cc72
koderDev <sapkotaanuj65@gmail.com>
2026-03-23 20:24:54 +0545

    Add files

---
+++ script.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = []
+
+
+function createParticle(x, y) {
+  return {
+    x,
+    y,
+    vx: (Math.random() - 0.5) * 3,
+    vy: (Math.random() - 0.5) * 3,
+    life: 0.75,
+    size: Math.random() * 2 + 4  
+  }
+}
+
+function drawParticle(ctx, p) {
+  ctx.globalAlpha = Math.pow(p.life, 2);    
+  ctx.fillStyle = `hsl(0, 100%, 60%)`
+  ctx.beginPath()
+  ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2)
+  ctx.fill()
+
+}
+
+function updateParticle(p) {
+  p.vy += 0.01
+  p.x += p.vx
+  p.y += p.vy
+  p.life -= 0.005
+}
+
+function loop(){
+  ctx.globalAlpha = 1
+  ctx.fillStyle = "#000"
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+  particles = particles.filter(p => p.life > 0)
+  particles.forEach(particle => {
+    updateParticle(particle)
+    drawParticle(ctx, particle)
+  })
+
+  requestAnimationFrame(loop)
+}
+
+loop();
+
+canvas.addEventListener("mousemove", (e) => {
+  for (let i = 0; i < 2; i++) {
+    particles.push(createParticle(e.clientX, e.clientY))
+  }
+})
+//createParticle(200,200);
+//drawParticle(ctx,createParticle(200,200));
