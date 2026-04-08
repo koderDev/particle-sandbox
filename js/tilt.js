8a4550146d6819d4c838098d31ea9a05d32d94cf
koderDev <sapkotaanuj65@gmail.com>
2026-04-08 06:54:47 +0545

    testint with scale of tilt

--- js/tilt.js
+++ js/tilt.js
@@ -2,7 +2,7 @@ function applyDeviceMotion(evnt){
     if(!tiltMode||!tiltPerms) return
     const accn=evnt.accelerationIncludingGravity
     if(!accn) return
-    const scale=0.05 //responsive banauna lai
+    const scale=0.09 //responsive banauna lai
     tiltGravX=-(accn.x||0)*scale
     tiltGravY=(accn.y||0)*scale
 }
